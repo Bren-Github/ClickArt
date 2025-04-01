fb3cec2b3823520ebf48f2563a480a1cd0f5fb42
Yenii <yenijimenez0709@gmail.com>
2025-04-01 12:47:23 -0500

    actualización

--- js/index.js
+++ js/index.js
@@ -1,14 +1,20 @@
 const user = JSON.parse(localStorage.getItem("user")) || [];
+const linkCerrar = document.querySelector("#link-cerrar")
+const linkIniciar = document.querySelector("#link-iniciar")
 
 function verificarUsuario (){
     
     for (let i = 0; i< user.length; i++) {
             if(user[i].logged == true){
                 console.log("Puedes cerrar sesión")
+                linkCerrar.style.display = "block"
+                linkIniciar.style.display = "none"
                 return
             }
     }
 
     console.log("Debes iniciar sesión")
+    linkCerrar.style.display = "none"
+    linkIniciar.style.display = "block"
 }
  verificarUsuario()
