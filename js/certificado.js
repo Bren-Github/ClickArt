d963bd6baf7e53e11df480520b1093ecfceff578
Yeni <yenijimenez0709@gmail.com>
2025-06-04 15:14:11 -0500

    certificado

--- js/certificado.js
+++ js/certificado.js
@@ -3,15 +3,21 @@ document.getElementById("descargarPDF").addEventListener("click", () => {
     console.log(certificado)
 
     const options = {
-        margin: 0.3,
+        margin: 0,
         filename: "ejemplo.pdf",
         image: { type: "pdf", quality: 0.98},
         html2canvas: {scale: 2},
         jsPDF: {
-            unit: "in",
-            format: "a4",
+            unit: "px",
+            format: [1000,550],
             orientation: "landscape"
         }
     }
-    html2pdf().set(options).from(certificado).save()
+    html2pdf().set(options).from(certificado).toPdf().get("pdf").then(
+        function(pdf){
+            const newWindow = window.open(pdf.output
+                ("bloburl", "_blank"))
+        }
+    );
+    
 })
